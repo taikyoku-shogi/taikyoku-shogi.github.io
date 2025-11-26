3e9e3f62aba60f50aa5ed86b2ff3e0c35648b844
s1050613 <83636592+s1050613@users.noreply.github.com>
2025-11-26 22:35:05 +1100

    Fix 404 page on GitHub Pages
    (hopefully)

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
 import dsv from "@rollup/plugin-dsv";
+import pc from "picocolors";
+import path from "node:path";
+import * as fs from "node:fs";
 
 export default defineConfig({
 	plugins: [
@@ -13,6 +16,14 @@ export default defineConfig({
 				previewMiddlewareFallback: "/404",
 			}
 		}),
-		dsv()
+		dsv(),
+		{
+			name: "copy-404-for-github-pages",
+			closeBundle() {
+				const dist = path.resolve("dist");
+				fs.copyFileSync(path.join(dist, "404", "index.html"), path.join(dist, "404.html"));
+				console.log(pc.green("✓ Copied 404/index.html to 404.html for GitHub Pages"));
+			}
+		}
 	],
 });
