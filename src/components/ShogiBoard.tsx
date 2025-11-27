6169bab356015f5a2ee3dd194b0de0d8f4451718
s1050613 <83636592+s1050613@users.noreply.github.com>
2025-11-28 08:52:41 +1100

    Play a click sound effect on each move

--- src/components/ShogiBoard.tsx
+++ src/components/ShogiBoard.tsx
@@ -1,10 +1,12 @@
-import { useCallback, useState } from "preact/hooks";
+import { useCallback, useRef, useState } from "preact/hooks";
 import Game from "../lib/Game";
 import { joinClasses, range } from "../lib/utils";
 import { Move, Player, Vec2 } from "../types/TaikyokuShogi"
 import styles from "./ShogiBoard.module.css";
 import * as vec2 from "../lib/vec2";
 import BoardSquare from "./BoardSquare";
+
+import pieceClickAudio from "../assets/pieceClick.mp3";
 
 export default function ShogiBoard({
 	game,
@@ -13,6 +15,8 @@ export default function ShogiBoard({
 	game: Game,
 	bottomPlayer?: Player
 }) {
+	const audioRef = useRef<HTMLAudioElement | null>(null);
+	
 	const currentPlayer = game.getCurrentPlayer();
 	
 	const [selectedSquare, setSelectedSquare] = useState<Vec2 | null>(null);
@@ -40,37 +44,44 @@ export default function ShogiBoard({
 		const move = moves!.find(move => vec2.equals(move.end, [x, y]));
 		console.log("making move:", move);
 		game.makeMove(move!);
+		audioRef.current!.play();
 		clearSelected();
 	}, [game, bottomPlayer, selectedSquare]);
 	
 	return (
-		<div
-			className={styles.board}
-			data-current-player={currentPlayer == Player.Sente? "sente" : "gote"}
-		>
-			{range(36).flatMap(y => (
-				range(36).map(x => {
-					const boardPos: [number, number] = calculateBoardPos([x, y]);
-					const piece = game.getSquare(boardPos);
-					
-					const isMoveTarget = moveTargets?.has(boardPos);
-					const canBeSelected = !selectedSquare && piece?.canMove(boardPos, game);
-					const isSelectedSquare = selectedSquare && vec2.equals(selectedSquare, boardPos);
-					
-					return (
-						<BoardSquare
-							x={x}
-							y={y}
-							piece={piece}
-							onClick={isMoveTarget? handleMoveClick : canBeSelected? handleClick : clearSelected}
-							className={joinClasses(
-								(isSelectedSquare || canBeSelected) && styles.selected,
-								isMoveTarget && styles.moveTarget
-							)}
-						/>
-					)
-				})
-			))}
-		</div>
+		<>
+			<audio
+				ref={audioRef}
+				src={pieceClickAudio}
+			></audio>
+			<div
+				className={styles.board}
+				data-current-player={currentPlayer == Player.Sente? "sente" : "gote"}
+			>
+				{range(36).flatMap(y => (
+					range(36).map(x => {
+						const boardPos: [number, number] = calculateBoardPos([x, y]);
+						const piece = game.getSquare(boardPos);
+						
+						const isMoveTarget = moveTargets?.has(boardPos);
+						const canBeSelected = !selectedSquare && piece?.canMove(boardPos, game);
+						const isSelectedSquare = selectedSquare && vec2.equals(selectedSquare, boardPos);
+						
+						return (
+							<BoardSquare
+								x={x}
+								y={y}
+								piece={piece}
+								onClick={isMoveTarget? handleMoveClick : canBeSelected? handleClick : clearSelected}
+								className={joinClasses(
+									(isSelectedSquare || canBeSelected) && styles.selected,
+									isMoveTarget && styles.moveTarget
+								)}
+							/>
+						)
+					})
+				))}
+			</div>
+		</>
 	);
 }
